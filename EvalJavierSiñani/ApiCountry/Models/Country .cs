053a56433007ab9ef232c80c46e941112496abc3
Javier Siñani Lijeron <a2019113929@estudiantes.upsa.edu.bo>
2021-03-17 18:31:07 -0400

    Modelo incorporado

---
+++ EvalJavierSiñani/ApiCountry/Models/Country .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiCountry.Models
+{
+    public class Country
+    {
+        [Key]
+        public string name { get; set; }
+        [Required]
+        public string capital { get; set; }
+        public int population { get; set; }
+        public List<double> latlng { get; set; }
+        public List<string> Timezones { get; set; }
+        public List<Currency> Currencies { get; set; }
+        public string Flag { get; set; }
+    }
+}
